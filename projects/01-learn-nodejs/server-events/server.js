aea6b64ab40a0b2b8e3c2ce27484b15cd26dfe32
marykasp <kasparian.mary@gmail.com>
2025-11-14 10:49:29 -0600

    exercise: set up response for sending temperature data to client on a keep-alive connection

--- projects/01-learn-nodejs/server-events/server.js
+++ projects/01-learn-nodejs/server-events/server.js
@@ -6,7 +6,27 @@ const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
   if (!req.url.startsWith("/temp/live")) {
+    // return static files
     return await handleFiles(req, res, __dirname);
+  } else if (req.url === "/temp/live") {
+    // set up server side stream of data
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    // send the temperature to client
+    setInterval(() => {
+      const temperature = getTemp();
+
+      // res.end would finish the response
+      res.write(
+        `data: ${JSON.stringify({
+          event: "temp-updated",
+          temp: temperature,
+        })}\n\n`,
+      );
+    }, 2000);
   }
 });
 
