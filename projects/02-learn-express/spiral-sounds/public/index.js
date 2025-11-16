3b614ef03e8cae0086a30f424e9ae721dc5046c6
marykasp <kasparian.mary@gmail.com>
2025-11-16 11:12:45 -0600

    exercise: create front end javaascript to fetch all products and render a product card, also generate the select genre options from the API endpoint

---
+++ projects/02-learn-express/spiral-sounds/public/index.js
@@ -0,0 +1,64 @@
+// ========== MENU TOGGLE ==========
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".header-menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+// ======== PRODUCT FETCHING =======
+async function getProducts(filter = {}) {
+  const queryParams = new URLSearchParams(filter);
+
+  const res = await fetch(`/api/products?${queryParams}`);
+  return await res.json();
+}
+
+// ========= PRODUCT RENDERING ========
+function renderProducts(products) {
+  const albumsContainer = document.getElementById("products-container");
+  const cards = products
+    .map((album) => {
+      return `
+      <div class="product-card">
+        <img src="./images/${album.image}" alt="${album.title}">
+        <h2>${album.title}</h2>
+        <h3>${album.artist}</h3>
+        <p>${album.price}</p>
+        <button class="add-btn">Add to Cart</button>
+        <p class="genre-label">${album.genre}</p>
+      </div>
+    `;
+    })
+    .join("");
+
+  albumsContainer.innerHTML = cards;
+}
+
+// ========= Genre Dropdown ========
+// populates the genre dropdown with available genres from the API
+
+async function populateGenreSelect() {
+  const res = await fetch("/api/products/genres");
+  const genres = await res.json();
+  const select = document.getElementById("genre-select");
+
+  genres.forEach((genre) => {
+    const option = document.createElement("option");
+    option.value = genre;
+    option.textContent = genre;
+    select.appendChild(option);
+  });
+}
+
+// INITIAL LOAD
+
+async function init() {
+  // fetch all products based on query params
+  const products = await getProducts();
+  // render the returned products as product cards
+  renderProducts(products);
+  // populate the genres in the select menu
+}
+
+init();
