8acdae03741f6dbb7734e8ef29c8c65ddf84a82d
marykasp <kasparian.mary@gmail.com>
2025-11-16 10:08:56 -0600

    exercise: add front end files for fullstack file app

--- projects/02-learn-express/spiral-sounds/server.js
+++ projects/02-learn-express/spiral-sounds/server.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.use(express.static("public"));
+
 app
   .listen(8000, () => console.log(`Server running on port: ${PORT}`))
   .on("error", (err) => {
