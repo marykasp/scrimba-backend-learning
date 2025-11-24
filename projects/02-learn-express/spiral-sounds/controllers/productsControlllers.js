a374a6bb8a1a497ab7f5c896ec523302536addeb
marykasp <kasparian.mary@gmail.com>
2025-11-24 09:25:04 -0600

    exercise: create utility db connection function, getGenres from the database and add to response object

--- projects/02-learn-express/spiral-sounds/controllers/productsControlllers.js
+++ projects/02-learn-express/spiral-sounds/controllers/productsControlllers.js
@@ -1,4 +1,22 @@
-export async function getGenres() {
+import { getDBConnection } from "../db/db";
+
+export async function getGenres(req, res) {
+  try {
+    // set up connect with db
+    const db = await getDBConnection();
+
+    //SQL query the genres
+    const query = `SELECT DISTINCT genre from products`;
+    const genres = await db.all(query);
+
+    // convert the object of genres into a string of genres
+    const genreStrings = genres.map((genre) => genre.genre);
+    res.json(genreStrings);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch genress", details: err.message });
+  }
   console.log("genres");
 }
 
